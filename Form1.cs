f97e205aa419c2e045f11f0068a75853ea1959ca
cupchaikin22 <ftgr3AAAd3@outlook.com>
2024-07-09 16:12:29 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIWbVYeVGL-BFsSyPmubTbbiMDl1Oii4oLxQ&s");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("during system operationan error was detected");
+        }
+    }
+}
